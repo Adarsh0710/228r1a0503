4b508c65c25ba89157eea6e6fe307b94113b4413
Adarsh Kumar Singh <145589188+Adarsh0710@users.noreply.github.com>
2024-03-08 19:19:23 +0530

    Operators

---
+++ operators.py
@@ -0,0 +1,48 @@
+'''opretor
+1.arithamatic
+2.logical
+3.comparision
+4.bitwise
+5.membership -- in, not in
+6.identity -- is ,not is
+7.Assigement
+
+'''
+
+
+n=2
+m=88
+print(m//n)
+print(m/n)
+print(2**3)
+print(n==m)
+a=10
+b=10
+print(a is b)
+b=20
+c=a
+print(a is b )#is c)
+#id(a)
+print(id(a))
+print(id(c))
+
+#is not is :-   it is use as the address
+
+x='helo'
+u='issi'
+z=x
+print("x is :" ,x is u)
+print("z is" ,z is x )
+k=['cse','ec']
+j=['cse','ec']
+print(k is j)
+print(j is n)
+
+
+
+# in or not in  it is use as the values of the which is an object
+
+a=[1,2,5,6,88,77]
+print(5 in a)
+print(2 not in a)
+
